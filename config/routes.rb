7154e48f19f353513c31aac1ad6b998f5d7029a0
Mounaim Hamim <02hamim@gmail.com>
2020-11-29 09:04:42 +0000

    added form fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-    root to: 'pages#home'
-    get 'about', to: 'pages#about'
-    get 'products', to: 'pages#products'
-    resources :contacts
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'products', to: 'pages#products'
+  resources :contacts
+  # we could've used the regular get 'contacts', to:'contacts#new' but we used a shortcut hashtag best practice :D 
+
 end
