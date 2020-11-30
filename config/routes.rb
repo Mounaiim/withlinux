c28cacdae2d284393cdf51cca8e2dbe346edb062
Mounaim Hamim <02hamim@gmail.com>
2020-11-30 10:45:07 +0000

    Added bootstrap to form

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'products', to: 'pages#products'
-  resources :contacts
+  get 'new', to: 'contacts#new' # resources :contacts then use /contacts/new
   # we could've used the regular get 'contacts', to:'contacts#new' but we used a shortcut hashtag best practice :D 
-
+  
 end
