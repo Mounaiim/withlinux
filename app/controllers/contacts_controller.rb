27c5e51944095861ba54b5f8df184099640e3020
Mounaim Hamim <02hamim@gmail.com>
2020-12-26 04:11:09 +0000

    Linked the contact page to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,21 @@
 class ContactsController < ApplicationController
-    
-  def new  #method called new refers to the page called contacts  
+    #method called new refers to the page called contacts  
+  def new  
+    @contact = Contact.new
   end
   
+  def create 
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Message successfuly sent. "
+    else
+      redirect_to new_contact_path, notice: "Message Aborted"
+    end
+end    
+    private 
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
     
 end
+    
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ActiveRecord::Base
+    # in my contacts table I already said that I want to record those attributes so rails does not require me to rewrite them in the class
+    #this the blueprint for our contacts basically (just an extensive explanation lololol)
+    #name 
+    #email
+    #comment
 end
+
+
+
+
+#the model file contains the blue print of each of our objects essentially every information captured by the app like users info (name, email, comments)
+#we dont have to write it the attributes here because rails does it for us
