aed2c6771ee7276473926fbae7bb39b58cda9c7f
Mounaim Hamim <02hamim@gmail.com>
2021-01-05 01:33:35 +0000

    Added mailer settings to environment file

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'whispering-refuge-85462.herokuapp.com',
+  :domain         => 'still-temple-44196.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
